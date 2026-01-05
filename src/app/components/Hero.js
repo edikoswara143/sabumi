e8502b2d4da6d373c5f384ef33d67fcdf8b8e58c
edi koswara <edikoswara143@gmail.com>
2026-01-05 15:16:51 +0700

    Hero

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,16 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
+import ProfileImage from './../../../public/Image/image.png';
 
 export default function Hero() {
     return (
-        <div className='bg-amber-500 h-60 w-full'>
-            <div className="">
+        <div className='h-96 w-full relative'>
+            <Image src={ProfileImage} alt="Description" className="w-full h-full object-cover bg-cover brightness-50" />
+            <div className="absolute inset-0">
+                <div className="">
 
-            </div>
-            <div className="flex items-center justify-center w-full py-20 flex-col">
-                <h1 className="font-bold text-2xl leading-relaxed">
-                    Lorem Ipsum</h1>
-                <p className="text-center leading-relaxed font-medium">
-                    Siap bantu siapa pun melanjutkan kebaikannya jadi lebih berdampak.</p>
+                </div>
+                <div className="flex items-center justify-center w-full pt-20 flex-col px-8 z-10">
+                    <h1 className="font-bold text-2xl leading-relaxed text-white">
+                        SABUMI FOUNDATION</h1>
+                    <p className="text-center leading-relaxed font-medium text-xs text-white">
+                        SABUMI FOUNDATION merupakan organisasi non-pemerintah (NGO) yang bergerak di bidang sosial kemanusiaan dan kepedulian lingkungan dengan prinsip keberlanjutan</p>
+                </div>
+
+                <div className="pt-8 w-full items-center justify-center mx-auto flex">
+                    <button className="bg-white text-black font-semibold py-2 px-4 rounded">
+                        Learn More
+                    </button>
+                </div>
             </div>
         </div>
     )
